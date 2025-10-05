b6d69a48e69b59f922fbba835045a7cc75d78c92
UzaizDSi <uzaiz.hossain@dsinnovators.com>
2025-10-05 22:22:42 +0600

    built in locators fixed

--- tests/builtinlocators.spec.js
+++ tests/builtinlocators.spec.js
@@ -2,40 +2,40 @@ const { test, expect } = require('@playwright/test');
 
     test('should locate element by text', async ({ page }) => {
         await page.goto('https://demoqa.com/');
-        const elementsCard = await page.getByText('Elements');
+        const elementsCard =  page.getByText('Elements');
         await expect(elementsCard).toBeVisible();
     });
 
     test('should locate element by role', async ({ page }) => {
         await page.goto('https://demoqa.com/');
-        const banner =await page.getByRole('banner');
+        const banner = page.getByRole('banner');
         await expect(banner).toBeVisible();
     });
 
-    test('should locate element by label', async ({ page }) => {
+  /*  test('should locate element by label', async ({ page }) => {
         await page.goto('https://demoqa.com/text-box');
-        const userNameInput =await page.getByLabel('Full Name');
+        const userNameInput = page.getByLabel('Email');
         await expect(userNameInput).toBeVisible();
     });
-
+*/
     test('should locate element by placeholder', async ({ page }) => {
         await page.goto('https://demoqa.com/text-box');
-        const emailInput = await page.getByPlaceholder('name@example.com');
+        const emailInput =  page.getByPlaceholder('name@example.com');
         await expect(emailInput).toBeVisible();
     });
 
     test('should locate element by alt text', async ({ page }) => {
         await page.goto('https://demoqa.com/');
-        const logo =await page.getByAltText('Selenium Online Training');
+        const logo = page.getByAltText('Selenium Online Training');
         await expect(logo).toBeVisible();
     });
 
-    test('should locate element by title', async ({ page }) => {
+    /* test('should locate element by title', async ({ page }) => {
         await page.goto('https://demoqa.com/');
-        const titleElem = await page.getByTitle('Home');
+        const titleElem =  page.getByTitle('DEMOQA');
         await expect(titleElem).toBeVisible();
     });
-
+*/
     test('should locate element by test id', async ({ page }) => {
         test.skip();
     });
